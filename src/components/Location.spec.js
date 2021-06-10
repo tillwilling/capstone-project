45672a7b208a0534a36b09418ba8368c19b3f810
Till Willing <tillwilling@gmail.com>
2021-06-10 16:43:44 +0200

    edit location.spec

--- src/components/Location.spec.js
+++ src/components/Location.spec.js
@@ -1,12 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import Location from './Location'
 
-    import {render, screen} from '@testing-library/react'
-    import Location from './Location'
-
-    describe('Location', () => {
-        it('renders', () => {
-            render(<Location image=”/some/path" text="some text" />)
-            expect(screen.getByText('some text')).toBeInTheDocument()
-            .... check for img ...
-        })
-    })
-    
+describe('Location', () => {
+  it('renders', () => {
+    render(<Location image="../images/garden2" text="Großer Garten" />)
+    expect(screen.getByText('Großer Garten')).toBeInTheDocument()
+  })
+})
