d1ceb870ebca6d7ca759cae73dd2211a91117a8b
Till Willing <tillwilling@gmail.com>
2021-06-14 15:58:42 +0200

    cleanup for new project

--- src/components/Location.spec.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Location from './Location'
-
-describe('Location', () => {
-  it('renders', () => {
-    render(<Location image="../images/garden2" text="Großer Garten" />)
-    expect(screen.getByText('Großer Garten')).toBeInTheDocument()
-  })
-})
